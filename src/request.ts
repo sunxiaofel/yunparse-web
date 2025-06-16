e9453f9bf525a72c87bab2fea42a068d6e60cf70
sunxiaofei <sunxiaofei1422@gmail.com>
2025-06-16 20:54:07 +0800

    fix：服务器接口地址

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.3:3001', // 你的接口基础地址
+  baseURL: 'https://192.168.1.3:3001', // 你的接口基础地址
   timeout: 30000,
 })
 
