e8ee4a14c4a118d7728c2ff975ba75bee57cdc39
sunxiaofei <sunxiaofei1422@gmail.com>
2025-06-20 16:28:10 +0800

    fix

--- src/layout/MyFooter.tsx
+++ src/layout/MyFooter.tsx
@@ -17,6 +17,17 @@ export default function MyFooter() {
         接口由看戏仔提供：
         <a href="https://api.kxzjoker.cn/#">看戏仔</a>
       </div>
+      <div>
+        本站使用
+        <a target="_blank" href="https://react.dev">
+          React
+        </a>
+        ➕
+        <a target="_blank" href="https://ant-design.antgroup.com">
+          Ant-design
+        </a>
+        开发
+      </div>
       <div>如有问题 请联系我 QQ：411223871</div>
     </Footer>
   )
